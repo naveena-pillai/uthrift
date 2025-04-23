d0805d511e7aed99c9f1bb6ed88928dcb5043fd5
dhanyades <dhanya.desai@gmail.com>
2025-04-23 18:30:10 -0400

    added more style features

--- uthrift/src/pages/OrderCompletePage.tsx
+++ uthrift/src/pages/OrderCompletePage.tsx
@@ -1,6 +1,15 @@
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+
+
 const OrderCompletePage = () => {
+    const { width, height } = useWindowSize()
+
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
+             <Confetti
+                width={width}
+                height={height} />
             <div className="bg-white shadow-lg rounded-2xl p-10 max-w-lg text-center space-y-6">
                 <h1 className="text-3xl font-bold text-green-700">Your Order is Complete!</h1>
                 <p className="text-gray-600 text-lg">
