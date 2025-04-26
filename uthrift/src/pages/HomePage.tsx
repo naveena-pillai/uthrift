c32c8a34136fcc8c7548e2e703570f7e156b1316
Savya Miriyala <msavya@gmail.com>
2025-04-26 19:49:44 -0400

    add usecontext

--- uthrift/src/pages/HomePage.tsx
+++ uthrift/src/pages/HomePage.tsx
@@ -1,2 +1,44 @@
 // Shows most popular items
 // When item is clicked, will have to display an overlay of its info with a checkout button, create component maybe
+import { useItemContext } from "../Context/ItemContext";
+import { useState } from "react";
+import Item from "../components/Item";
+import ItemCard from "../components/ItemCard";
+import type { ItemCardProps } from "../components/ItemCard"; 
+<Item key={index} name={item.name} thumbnail={item.thumbnail} price={item.price} onClick={() => setSelectedItem(item)} />
+
+const items: ItemCardProps[] = [
+//add here??
+];
+
+const HomePage = () => {
+  const { setSelectedItem } = useItemContext();
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-6">
+        {items.map((item: ItemCardProps, index) => (
+          <Item
+            key={index}
+            name={item.name}
+            thumbnail={item.thumbnail}
+            price={item.price}
+            onClick={() => setSelectedItem(item)} // <--- set context when clicked
+          />
+        ))}
+      </div>
+
+      <ItemCard /> 
+    </>
+  );
+}
+
+export default HomePage;
+
+
+//add later???
+    /*const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget) {
+            onClose();
+        }
+    }*/
