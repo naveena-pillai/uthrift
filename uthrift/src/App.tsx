1c8bad9bc4316f218eb81437501df411c51c330c
naveena-pillai <71680517+naveena-pillai@users.noreply.github.com>
2025-04-25 21:37:32 -0400

    Update App.tsx

--- uthrift/src/App.tsx
+++ uthrift/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
