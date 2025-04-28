a62322be39566075deafef3c1e3985659444b4e6
naveena-pillai <naveena.t.pillai@gmail.com>
2025-04-28 00:25:09 -0400

    fixed navbar and added to files

--- uthrift/src/pages/CategoryPage.tsx
+++ uthrift/src/pages/CategoryPage.tsx
@@ -1,2 +1,11 @@
 // Displays all items from a given category
 // When item is clicked, will have to display an overlay of its info with a checkout button, create component maybe
+import Navbar from "../components/Navbar";
+
+export default function CategoryPage() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
