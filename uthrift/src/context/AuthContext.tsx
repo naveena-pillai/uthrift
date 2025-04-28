961c781247a9e43b8052fbe0153b697dab09d04f
naveena-pillai <naveena.t.pillai@gmail.com>
2025-04-28 00:01:12 -0400

    profile page etc

--- uthrift/src/context/AuthContext.tsx
+++ uthrift/src/context/AuthContext.tsx
@@ -1,10 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { User, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/firebase";
+import { signOut } from "firebase/auth";
 
 interface AuthContextType {
-  currentUser: User | null;
-  
+  currentUser: any;
+  logout: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -20,11 +21,16 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     return () => unsubscribe();
   }, []);
 
-  return (
-    <AuthContext.Provider value={{ currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  const logout = async () => {
+    return signOut(auth);
+  };
+
+  const value = {
+    currentUser,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => {
